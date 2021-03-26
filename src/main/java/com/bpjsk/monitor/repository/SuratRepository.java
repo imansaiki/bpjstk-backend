9bd994754a1b569b5c6ccc1ddb51174a5e8d2f81
Iman Setya A <v-iman.setya@anteraja.id>
2021-03-26 10:31:43 +0700

    fix: fix typo on Surat Repository

--- src/main/java/com/bpjsk/monitor/repository/SuratRepository.java
+++ src/main/java/com/bpjsk/monitor/repository/SuratRepository.java
@@ -1,6 +1,7 @@
 package com.bpjsk.monitor.repository;
 
+import com.bpjsk.monitor.model.Surat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SuratRepository extends JpaRepository<SuratRepository,Long> {
+public interface SuratRepository extends JpaRepository<Surat,Long> {
 }
